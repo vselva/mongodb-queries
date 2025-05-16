9ecbdf8719c07e4711883de64e3dc5f9ef31e007
Selvakumar <vselva@gmail.com>
2025-05-16 17:21:30 +0530

    Create allow-disk-usage.js

---
+++ Aggregation/allow-disk-usage.js
@@ -0,0 +1,13 @@
+// In MongoDB, the allowDiskUse option in the aggregate() method enables the server 
+// to write data to temporary disk files if memory limits are exceeded 
+// during the aggregation operation.
+
+use aggregate
+
+db.products.aggregate(
+    [
+        { $match: { category: { $in: [ "clothing", "books" ] } } },
+        { $sort: { name: 1 } },
+        { $group: { _id: "$category" } }
+    ], { allowDiskUse: true }
+);
